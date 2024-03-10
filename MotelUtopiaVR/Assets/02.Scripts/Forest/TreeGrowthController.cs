b735b9895e53fae469788c555f753e42c191d1f6
rachel0402 <hrp0112@gmail.com>
2024-03-10 22:05:37 +0900

    feat. GameMenu settings

---
+++ MotelUtopiaVR/Assets/02.Scripts/Forest/TreeGrowthController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeGrowthController : MonoBehaviour
+{
+    public Material treeMaterial; // 나무 Material
+    private float growthFactor = 0f; // 현재 성장 정도 (0에서 1 사이)
+    public float growthSpeed = 0.005f; // 성장 속도 조절, 매 프레임마다 적용될 값
+
+    private void Update()
+    {
+        // 나무가 최대 성장치에 도달하지 않았다면 성장
+        if (growthFactor < 1f)
+        {
+            growthFactor += growthSpeed * Time.deltaTime; // 성장 속도를 프레임 레이트에 독립적으로 조절
+            treeMaterial.SetFloat("_GrowthFactor", growthFactor);
+        }
+    }
+}
+
---
+++ MotelUtopiaVR/Assets/02.Scripts/Menu/SceneFader.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneFader : MonoBehaviour
+{
+    public Image fadeOutUIImage;
+    public float fadeSpeed = 0.8f;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject); // 이 오브젝트를 씬 전환 시에도 파괴되지 않도록 함
+    }
+
+    public enum FadeDirection
+    {
+        In, //Alpha 1 to 0
+        Out //Alpha 0 to 1
+    }
+
+    // 시작할 때 바로 페이드 인 시작
+    void Start()
+    {
+        StartCoroutine(Fade(FadeDirection.In));
+    }
+
+    // 씬을 로드하고 페이드 아웃하기
+    public void LoadScene(string sceneName)
+    {
+        StartCoroutine(FadeAndLoadScene(FadeDirection.Out, sceneName));
+    }
+
+    // 페이드 인/아웃 처리
+    IEnumerator Fade(FadeDirection fadeDirection)
+    {
+        float alpha = (fadeDirection == FadeDirection.Out) ? 0 : 1;
+        float fadeEndValue = (fadeDirection == FadeDirection.Out) ? 1 : 0;
+        if (fadeDirection == FadeDirection.Out)
+        {
+            while (alpha < fadeEndValue)
+            {
+                SetColorImage(ref alpha, fadeDirection);
+                yield return null;
+            }
+            fadeOutUIImage.enabled = false; // 페이드 아웃 후 이미지 비활성화
+        }
+        else
+        {
+            fadeOutUIImage.enabled = true; // 페이드 인 전 이미지 활성화
+            while (alpha > fadeEndValue)
+            {
+                SetColorImage(ref alpha, fadeDirection);
+                yield return null;
+            }
+        }
+    }
+
+    // 씬 로드와 함께 페이드 처리
+    IEnumerator FadeAndLoadScene(FadeDirection fadeDirection, string sceneName)
+    {
+        yield return Fade(fadeDirection);
+        SceneManager.LoadScene(sceneName);
+        // 새 씬에서도 페이드 인이 작동하려면, 새 씬에도 SceneFader 오브젝트가 있어야 합니다.
+    }
+
+    // 이미지의 색상 설정
+    void SetColorImage(ref float alpha, FadeDirection fadeDirection)
+    {
+        fadeOutUIImage.color = new Color(fadeOutUIImage.color.r, fadeOutUIImage.color.g, fadeOutUIImage.color.b, alpha);
+        alpha += Time.deltaTime * (1.0f / fadeSpeed) * ((fadeDirection == FadeDirection.Out) ? 1 : -1);
+    }
+}
+
