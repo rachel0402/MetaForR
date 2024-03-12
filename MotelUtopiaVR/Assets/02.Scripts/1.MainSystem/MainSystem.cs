65333e60da5b6be144303375d65e3b135574f098
shinhyejune <83018010+shinhyejune@users.noreply.github.com>
2024-03-12 23:54:30 +0900

    feat. FadeEffect

---
+++ MotelUtopiaVR/Assets/02.Scripts/1.MainSystem/MainSystem.cs
@@ -0,0 +1,26 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class MainSystem : CustomSingleton<MainSystem>
+//{
+//   // FadeEffect fadeEffect;
+
+
+//    public void Test()
+//    {
+//        Debug.Log("test");
+//    }
+//    private void Allocate()
+//    {
+//       // fadeEffect = gameObject.AddComponent<FadeEffect>();
+//    }
+//    private void Initialze()
+//    {
+//        Allocate();
+//    }
+//    private void Start()
+//    {
+//        Initialze();
+//    }
+//}
